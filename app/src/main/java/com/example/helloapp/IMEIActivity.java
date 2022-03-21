8aa92e932b282f7b0cfa39b04ff4b5f315978c8b
Serg Studzinskiy <61849458+nono002@users.noreply.github.com>
2022-03-21 22:21:00 +0200

    + add IMEI activity (androidID instead IMEI) - no any minds

--- app/src/main/java/com/example/helloapp/IMEIActivity.java
+++ app/src/main/java/com/example/helloapp/IMEIActivity.java
@@ -1,9 +1,15 @@
 package com.example.helloapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
@@ -11,11 +17,16 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.provider.Settings;
+import android.provider.Settings.System;
 
-public class IMEIActivity extends AppCompatActivity implements OnClickListener {
+public class IMEIActivity extends Activity implements OnClickListener {
 
     Button btnActIMEI;
     TextView textView;
+
+    Context context;
+    TelephonyManager tm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,20 +35,29 @@ public class IMEIActivity extends AppCompatActivity implements OnClickListener {
 
         btnActIMEI = (Button) findViewById(R.id.btnActIMEI);
         btnActIMEI.setOnClickListener(this);
+
+        context = this;
+        //tm = (TelephonyManager) getSystemService(Context.LOCATION_SERVICE);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
-    public void onClick(View v){
+    public void onClick(View v) {
         Intent intent;
 
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btnActIMEI:
                 // part "get IMEI"
 
                 //Have an  object of TelephonyManager
-                TelephonyManager tm =(TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-                //Get IMEI Number of Phone  //////////////// for this example i only need the IMEI
-                String IMEINumber=tm.getDeviceId();
+                //TelephonyManager tm = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+                //String androidID = System.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+
+               /* String IMEINumber = "";
+                if (null != tm){
+                    //Get IMEI Number of Phone  //////////////// for this example i only need the IMEI
+                    IMEINumber = tm.getDeviceId(); //.getImei();
+                }*/
 
                 /*
                 TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
@@ -57,10 +77,30 @@ public class IMEIActivity extends AppCompatActivity implements OnClickListener {
                 else
                 {
                     imei="20"; //telephonyManager.getDeviceId();
-                }*/
+                }*//*
                 textView = (TextView) findViewById(R.id.textViewIMEI);
-                textView.setText(IMEINumber);
+                //TelephonyManager tm = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+                try {
+                    tm = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        textView.setText((CharSequence) tm.getImei(1));
+                    }
+                } catch (Exception e) {
+                    //e.getMessage();
+                    //.printStackTrace();
+                    textView.setText(e.getMessage());
+                }*/
+
+                String androidID = System.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+                textView = (TextView) findViewById(R.id.textViewIMEI);
+                textView.setText(androidID);
                 break;
         }
+    }
+
+    public String getIMEI(Activity activity) {
+        TelephonyManager telephonyManager = (TelephonyManager) activity
+                .getSystemService(Context.TELEPHONY_SERVICE);
+        return telephonyManager.getDeviceId();
     }
 }
